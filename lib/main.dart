cb5835aeeed710976aff8e578a86d79c68e7af5d
gfrancoo <gianfranco.aguirre.cruz@gmail.com>
2020-11-27 08:18:39 -0500

    Change routes

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:minimarket/pages/login_page.dart';
-import 'package:minimarket/pages/home_page.dart';
 import 'package:minimarket/pages/productos_page.dart';
 import 'package:minimarket/pages/ventas_page.dart';
 import 'package:minimarket/util/constants.dart';
@@ -15,7 +14,6 @@ class MyApp extends StatelessWidget {
       theme: themeDataApp,
       initialRoute: "/login",
       routes: {
-        "/": (BuildContext context) => Welcome(),
         "/login": (BuildContext context) => LoginScreen(),
         "/productos": (BuildContext context) => ProductosPage(),
         "/ventas": (BuildContext context) => VentasPage()
